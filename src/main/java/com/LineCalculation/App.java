95f658779e1d004e3e026619254cafa899cb9aca
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-11-17 18:58:04 +0530

    Added: Welcome message to the program

---
+++ src/main/java/com/LineCalculation/App.java
@@ -0,0 +1,9 @@
+package com.LineCalculation;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Line Comparison Computation" );
+    }
+}
